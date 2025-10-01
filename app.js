bf0348428632a7ee996d456b26f5d44258bfb0d2
Legend Paul <qamunyamaina950@gmail.com>
2025-10-01 21:19:41 +0300

    Create header

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const pool = require("./db/pool");
+const path = require("node:path");
 require("dotenv").config();
+const homeRouter = require("./routes/homeRouter");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", homeRouter);
 
 pool.connect((err, client, release) => {
     if (err) throw err;

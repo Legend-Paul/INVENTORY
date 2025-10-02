01e37fef54a0324950205d38e3e78c9560822bb5
Legend Paul <qamunyamaina950@gmail.com>
2025-10-02 15:45:52 +0300

    Display count of category and items and sum of price and quantity in homepage

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -5,7 +5,17 @@ const query = require("../db/query");
 const getAllCategories = async (req, res) => {
     try {
         const categories = await query.getAllCategories();
-        res.render("home", { categories });
+        const categoryCount = await query.getCategorycount();
+        const items = await query.getItemCount();
+        const quantities = await query.getTotalQuantity();
+        const price = await query.getTotalPrice();
+        res.render("home", {
+            categories,
+            categoryCount,
+            items,
+            quantities,
+            price,
+        });
     } catch (error) {
         throw error;
     }

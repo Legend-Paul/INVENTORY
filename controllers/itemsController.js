6e8017c8a2c734086994772f0165f4e3be8221a7
Legend Paul <qamunyamaina950@gmail.com>
2025-10-02 15:20:22 +0300

    Add getting all items

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -3,11 +3,15 @@ const query = require("../db/query");
 const getCategoryItems = async (req, res) => {
     try {
         const categoryId = req.query.category;
-        const items = await query.getItemsByCategoryId(categoryId);
-        const category = await query.getCategoryById(categoryId);
+        if (categoryId) {
+            const items = await query.getItemsByCategoryId(categoryId);
+            const category = await query.getCategoryById(categoryId);
 
-        const categoryName = category[0].name;
-        res.render("items", { items, categoryName });
+            const categoryName = category[0].name;
+            return res.render("items", { items, categoryName });
+        }
+        const items = await query.getAllItems();
+        res.render("items", { items, categoryName: "All Items" });
     } catch (err) {
         throw err;
     }

ed5c6f7ca75b339d2b21f3b6401d55b770945368
Legend Paul <qamunyamaina950@gmail.com>
2025-10-03 03:08:39 +0300

    Add delete item

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -6,6 +6,14 @@ const deleteCategory = async (req, res) => {
     res.redirect("/");
 };
 
+const deleteItem = async (req, res) => {
+    const id = req.params.id;
+    const categoryId = req.query.category;
+    await query.deleteItem(id);
+    res.redirect(`/items?category=${categoryId}`);
+};
+
 module.exports = {
     deleteCategory,
+    deleteItem,
 };
--- routes/deleteRouter.js
+++ routes/deleteRouter.js
@@ -4,4 +4,6 @@ const deleteController = require("../controllers/deleteController");
 const deleteRouter = express.Router();
 deleteRouter.get("/category/:id", deleteController.deleteCategory);
 
+deleteRouter.get("/item/:id", deleteController.deleteItem);
+
 module.exports = deleteRouter;
